2756292890ad10ad3135b4bc43848a86bf9e03fa
carleondel <carleondel@gmail.com>
2024-09-04 19:50:29 +0200

    reddit_trials.py created. API working checked

---
+++ src/reddit_trials.py
@@ -0,0 +1,38 @@
+import praw
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+# Cargar variables de entorno desde .env
+load_dotenv()
+
+
+# Credenciales para acceder a la API de Reddit
+REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
+REDDIT_SECRET = os.getenv('REDDIT_SECRET')
+REDDIT_USER_AGENT = os.getenv('REDDIT_USER_AGENT')
+REDDIT_USERNAME = os.getenv('REDDIT_USERNAME')
+REDDIT_PASSWORD = os.getenv('REDDIT_PASSWORD')
+
+
+
+# Configuración de Reddit
+reddit = praw.Reddit(
+    client_id=REDDIT_CLIENT_ID,
+    client_secret=REDDIT_SECRET,
+    user_agent=REDDIT_USER_AGENT,
+    username=REDDIT_USERNAME,
+    password=REDDIT_PASSWORD 
+)
+
+
+subreddit = reddit.subreddit('mathmemes')
+
+for submission in subreddit.top(limit=10):
+    print(submission.title)
+    print('Score:', submission.score)
+    print('Url:', submission.url)
+    created_utc = submission.created_utc
+    created_date = datetime.utcfromtimestamp(created_utc).strftime('%Y-%m-%d %H:%M:%S')
+    print('Date:', created_date)
+    print()
